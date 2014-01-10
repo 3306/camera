46d9d5a0947060002811f95bde4c2a2101f6f40e
王焕宇 <why490078184@gmail.com>
2014-01-10 13:06:51 +0800

    完成数据库的设计，新建数据库连接层

---
+++ Server/ServerDLL/DBHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace ServerDLL
+{
+    class DBHelper
+    {
+        private SqlConnection conn;
+        public SqlConnection connection
+        {
+            get 
+            {
+                conn = new SqlConnection();
+                conn.ConnectionString = ConfigurationManager.ConnectionStrings["SchoolMonitor"].ConnectionString;
+                return conn;
+            }
+        }
+    }
+}
